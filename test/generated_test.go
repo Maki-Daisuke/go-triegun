ca6b4b53733e1c8e00a1cc56482ea71371ace5b8
Daisuke (yet another) Maki <maki.daisuke@gmail.com>
2015-11-23 01:38:43 +0900

    Implemented `generate` to output code tentatively.

---
+++ test/generated_test.go
@@ -0,0 +1,28 @@
+package main
+
+import "testing"
+
+func TestGenerate(t *testing.T) {
+	for _, it := range []string{
+		"AppleCoreMedia", // iOS native quick time player
+		"Apple-PubSub",   // RSS reader for screen saver?
+		"cloudd/",        // iCloud daemon
+		"itunesstored",
+		"gamed/", // Probably, Apple Game Center
+		"com.apple.geod",
+		"com.apple.invitation-registration",
+		"com.apple.Maps", // Map app?
+		"ocspd/",         // Mac OS X's ocspd, verifying certificate validity
+	} {
+		if !MatchUA(it) {
+			t.Errorf(`should match against %q, but didn't`, it)
+		}
+	}
+
+	if MatchUA(`hogeFuga`) {
+		t.Error(`should not match against "hogeFuga", but did`)
+	}
+	if MatchUA(`com.apple.geodd`) {
+		t.Error(`should not match against "com.apple.geodd", but did`)
+	}
+}
